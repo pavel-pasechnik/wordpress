6ba54e14a354015c5e0cfd37d5162091896c3485
Pavel <Pavel.Pasechnik@gmail.com>
2024-08-21 16:58:47 +0300

    added templates

--- themes/krishna-academy/functions.php
+++ themes/krishna-academy/functions.php
@@ -1,26 +1,48 @@
 <?php
-/**
- * Functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package ale
- * @since 1.0.0
- */
 
-/**
- * Enqueue the CSS files.
- *
- * @since 1.0.0
- *
- * @return void
- */
-function ale_styles() {
+#include get_parent_theme_file_path('inc/helpers.php');
+
+
+# insert styles
+add_action('wp_enqueue_scripts', 'krishna_academy_enqueue_styles');
+
+function krishna_academy_enqueue_styles()
+{
 	wp_enqueue_style(
-		'ale-style',
-		get_stylesheet_uri(),
-		[],
-		wp_get_theme()->get( 'Version' )
+		'krishna-academy-normalize',
+		get_stylesheet_uri('https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/3.0.0/modern-normalize.min.css'),
+	);
+	wp_enqueue_style(
+		'krishna-academy-reset',
+		get_parent_theme_file_uri('assets/css/reset.css'),
+		array(),
+		wp_get_theme()->get('Version'),
+		'all'
+	);
+	wp_enqueue_style(
+		'krishna-academy-style',
+		get_stylesheet_uri()
 	);
 }
-add_action( 'wp_enqueue_scripts', 'ale_styles' );
+
+# Add style in to Designer
+add_action('after_setup_theme', 'krishna_academy_setup');
+
+function krishna_academy_setup()
+{
+	add_editor_style(get_stylesheet_uri());
+}
+
+# Add javascript
+add_action('wp_enqueue_scripts', 'krishna_academy_enqueue_scripts');
+
+function krishna_academy_enqueue_scripts()
+{
+	wp_enqueue_script(
+		'krishna_academy-menu',
+		get_parent_theme_file_uri('assets/js/index.js'),
+		array(),
+		wp_get_theme()->get('Version'),
+		true
+	);
+}
---
+++ themes/krishna-academy/patterns/example.php
@@ -0,0 +1,9 @@
+<?php
+register_block_pattern(
+	'krishnaacademy/example',
+	array(
+		'title'       => __('Example Pattern', 'krishnaacademy'),
+		'description' => _x('Description of your pattern.', 'Block pattern description', 'krishnaacademy'),
+		'categories'  => array('text')
+	)
+);
